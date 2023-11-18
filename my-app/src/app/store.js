4ebd7400f7a7a21711469a93d3cff6f185ddb5d0
Dwayne Lacey <dwaynelacey.acm@gmail.com>
2023-11-18 17:10:57 -0500

    Updated store to use drum machine reducer

--- my-app/src/app/store.js
+++ my-app/src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import machineReducer from '../features/machine/machineSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    machine: machineReducer,
   },
 });
