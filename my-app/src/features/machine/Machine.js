34576b4e1e0d8dd02acf833e834df3471dff9e8b
Dwayne Lacey <dwaynelacey.acm@gmail.com>
2023-11-22 19:31:54 -0500

    Added Drum Button component to build buttons

--- my-app/src/features/machine/Machine.js
+++ my-app/src/features/machine/Machine.js
@@ -8,10 +8,14 @@ export function Machine() {
   const sourceText = useSelector(selectSourceText);
   
   return (
-    // Props to pass in for buttons, soundOn, buttonName, soundSrc, buttonKey
+    // Props to pass in for buttons, buttonName, soundSrc, buttonKey
     <div id="drum-machine">
       <div id="button-container" className={styles.buttonContainer}>
-        <DrumButton />
+        <DrumButton 
+        soundSrc="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+        buttonKey="Q"
+        buttonName="Heater-1"
+        />
       </div>
       <div className={styles.displayContainer}>
         <h1 className={styles.display} id="display">
